042d6c65a896a02b3cc7f08431ebcc6db79f999d
wavepig <2764306748@qq.com>
2024-06-27 22:59:33 +0800

    feat: cobra

--- cmd/chat/main.go
+++ cmd/chat/main.go
@@ -3,12 +3,31 @@ package main
 import (
 	"chat-server/internal/chat"
 	"fmt"
+	"github.com/spf13/cobra"
 )
 
+func newServerCommand() *cobra.Command {
+	//var configPath string
+	cmd := &cobra.Command{
+		Use:     "start",
+		Short:   "start chat server",
+		Example: `./chat start`,
+		Run: func(cmd *cobra.Command, args []string) {
+			server := chat.NewServer()
+			err := server.Run()
+			if err != nil {
+				fmt.Println("start server error: ", err)
+			}
+		},
+	}
+
+	//cmd.PersistentFlags().StringVarP(&configPath, "configPath", "c", "", "configuration file path for chat server")
+	return cmd
+}
+
 func main() {
-	server := chat.NewServer()
-	err := server.Run()
-	if err != nil {
-		fmt.Println("start server error: ", err)
+	root := newServerCommand()
+	if err := root.Execute(); err != nil {
+		panic(err)
 	}
 }
